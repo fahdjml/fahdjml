74ac6e1d61d515a0968ee3da4ac13b437fed15af
FAHAD C JAMAL <116793797+fahdjml@users.noreply.github.com>
2023-01-12 09:58:24 +0530

    Create pgm25.py

---
+++ pgm25.py
@@ -0,0 +1,6 @@
+i t_peri = lambda p,q,r : p + q + r
+r_area = lambda len, ht : len*ht
+s_area = lambda side : side*side
+print("Perimeter of Triangle (10,20,15) is:", t_peri(10,20,15))
+print("Area of Rectangle (30,20) is:", r_area(30,20))
+print("Area of square (5) is:", s_area(5))
